d9b81e9007cc298bf4f1843a35cc1c7458c439d2
liutz <1342677359@qq.com>
2021-01-21 18:18:29 +0800

    init

---
+++ src/main/resources/dashboard/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div v-if="this.step===1">
+      <Step1/>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import Step1 from "./components/Step1"
+export default {
+  name: 'app',
+  components: {
+    Step1
+  },
+  data() {
+    return {
+      step: 1,
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main/resources/dashboard/src/components/Step1.vue
@@ -0,0 +1,70 @@
+<template>
+<div>
+    <el-row>
+        <!-- 添加节点 -->
+        <el-col :span="8">
+            <h3>添加redis节点</h3>
+            <el-form ref="node" :model="node" label-width="65px">
+                <el-form-item label="name" prop="name">
+                    <el-input v-model="this.node.name" />
+                </el-form-item>
+                <el-form-item label="host" prop="host">
+                    <el-input v-model="this.node.host" />
+                </el-form-item>
+                <el-form-item label="port" prop="port">
+                    <el-input v-model="this.node.port" />
+                </el-form-item>
+                <el-form-item label="password" prop="password">
+                    <el-input v-model="this.node.password" />
+                </el-form-item>
+                <el-form-item>
+                    <el-button plain @click="addNode">检查</el-button>
+                    <el-button plain>添加</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+
+        <!--  -->
+        <el-col :span="14">
+
+        </el-col>
+
+    </el-row>
+</div>
+</template>
+
+<script>
+export default {
+    name: "step1",
+    data() {
+        return {
+            prop: {
+                "server.port":"",
+                groups: [],
+                nodes:[],
+            },
+            group: {
+                groupName: "",
+                groupIndex: "",
+                nodes:[]
+            },
+            node: {
+                nodeIndex:"",
+                nodeName:"",
+                host:"",
+                port:"",
+                password:"",
+            }
+        }
+    },
+    methods: {
+        addNode() {
+            let node = this.node;
+            this.prop.nodes.push(node);
+        },
+        resetNode(n) {
+            console.log(this.prop);
+        }
+    }
+}
+</script>
